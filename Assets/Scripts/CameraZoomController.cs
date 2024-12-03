630f9c9adbfefbda656ff2698ae705cd8d5d59a0
TalmonG <118438762+TalmonG@users.noreply.github.com>
2024-12-03 11:30:37 +0000

    save

--- Assets/Scripts/CameraZoomController.cs
+++ Assets/Scripts/CameraZoomController.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class CameraZoomController : MonoBehaviour
 {
 
-    public GameObject cineCam;
-    public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GameObject CineCam = GameObject.FindGameObjectWithTag("CineCam");
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Player.playerSizeCounter > 30)
+        {
+            Debug.Log("Zoom Out");
+        }
     }
 }
